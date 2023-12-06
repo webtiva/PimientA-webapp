cd8dc826dfe611af36eb1f4243e67833647a8c8a
Carlos Ramos <carlos.dg147@gmail.com>
2023-12-06 11:43:10 -0400

    Creando la migración (tabla) y el modelo (sin nada aun) para la tabla programa_analiticos.

---
+++ database/migrations/2023_12_06_152722_create_programa_analiticos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programa_analiticos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('materia');
+            $table->smallInteger('n_hojas');
+            $table->string('carrera')->default('Física');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programa_analiticos');
+    }
+};
