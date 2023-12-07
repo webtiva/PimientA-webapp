53a186d0e68c68004577cb91daec797ada51fbb3
Carlos Ramos <carlos.dg147@gmail.com>
2023-12-07 18:59:11 -0400

    UI-seeker, se añadió un alert en la página del buscador, el alert avisa si la emisión ha sido exitoso.

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,5 @@ Route::post('/programa-analitico/emision', function(Request $request) {
     $emision_pa->fecha_emision = Carbon::create($request->input('fecha'));
     $emision_pa->programa_analitico_id = $request->input('programa_analitico_id');
     $emision_pa->save();
-    return "se guardo!";
+    return redirect('/')->with('status', '¡Emisión del programa analítico exitoso!');
 });
