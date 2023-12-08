bb0933dc95989a443b0fc5b142e2e785ef969097
Carlos Ramos <carlos.dg147@gmail.com>
2023-12-08 13:01:52 -0400

    UI:doc-pdf-pa se arregló el html y css de está pagina porque al descargar el pdf, el formato estaba desordenado.
    El problema que se detecto fue, el componene utilizado para renderizar el pdf (dompdf) su interprete del css es limitado, no reconoce flex, text-align:end, ademas viendo su documentación hay una forma de hacer saltos de página con css, por lo que no se necesita definir el alto de la página.

--- resources/views/doc-pdf-pa.blade.php
+++ resources/views/doc-pdf-pa.blade.php
@@ -3,46 +3,53 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
     @page {
             margin: 0cm 0cm;
-            font-family: Arial;
+            font-family: sans-serif;
         }
-    .document {
-      width: 21.5cm;
-      font-family: sans-serif;
-    }
+
     .page {
-      height: 28cm;
-      border: dashed gray 1px;
-      padding: 0.1cm;
-      overflow: hidden;
+      /* border: dashed gray 1px; */
+      position: relative;
     }
     .page > img {
       width: 100%;
+      position: absolute;
     }
     .end-page {
       padding: 3cm;
     }
     .container-datos {
-      border: solid black 1px;
-      display: flex;
+      padding: 0.5cm;
+      border: solid black 1px;     
+      position: relative;
+      width: 100%;
+      height: 8cm;
     }
     .col-sides {
-      width: 1.5cm;
-      padding: 0.8cm 0.4cm;
+      width: 10%;
+      position: absolute;
+      height: 100%;
+    }
+    .col-medium {
+      width: 80%;
+      position: absolute;
+      height: 100%;
+      left: 10%;
     }
     img.logo {
       width: 100%;
-    }
-    .col-medium {
-      padding: 0.8cm 0cm;
     }
     .datos-institucion {
       text-align: center;
       font-weight: bolder;
     }
     .datos-solicitante {
-      padding-top: 0.3cm;
+      padding-top: 1cm;
     } 
     h1, h2 {
       padding: 0;
@@ -57,6 +64,7 @@
     p {
       margin: 0;
       padding: 0;
+      margin-bottom: 5px;
     }
     .field {
       font-weight: bold;
@@ -64,27 +72,42 @@
       width: 2cm;
     }
     .datos-n-hojas {
-      text-align: end;
+      width: 100%;
       font-weight: bolder;
+      text-align: right;
     }
     .fecha-emision {
+      padding-top: 1cm;
       text-align: center;
       font-size: 14px;
     }
     .nombre-sol {
       text-transform: uppercase;
+      display: inline-block;
     }
   </style>
+  <style>
+    .page-break {
+        page-break-after: always;
+    }
+    </style>
+
 
 </head>
 <body>
-  <div class="document">
+  {{-- <div class="document"> --}}
     <div class="page">
       <img src="{{public_path('assets/img/example-images-pa/page-0001.jpg')}}" alt="">
     </div>
+
+    <div class="page-break"></div>
+
     <div class="page">
       <img src="{{public_path('assets/img/example-images-pa/page-0002.jpg')}}" alt="">
     </div>
+
+    <div class="page-break"></div>
+
     <div class="page end-page">
       <div class="container-datos">
         <div class="col-sides">
@@ -99,9 +122,9 @@
             <h2>U.M.S.S.</h2>
           </div>
           <div class="datos-solicitante">
-            <p><span class="field">Nombre:</span> <span class="nombre-sol"> Quispe reinaga</span></p>
-            <p><span class="field">Carrera:</span> Licenciatura en Ing. Informática</p>
-            <p><span class="field">Materia:</span> Física Básica II (2006019)</p>
+            <p><span class="field">Nombre: </span><span class="nombre-sol">Quispe reinaga</span></p>
+            <p><span class="field">Carrera:</span><span style="display: inline-block">Licenciatura en Ing. Informática</span></p>
+            <p><span class="field">Materia:</span><span style="display: inline-block">Física Básica II (2006019)</span></p>
           </div>
           <div class="datos-n-hojas">
             <p>Fojas: 5</p>
@@ -110,11 +133,11 @@
             Cochabamba, 17 de agosto de 2023
           </div>
         </div>
-        <div class="col-sides">
+        <div class="col-sides" style="right: 0.5cm">
           <img class="logo" src="{{public_path('assets/img/logo-fcyt.png')}}" alt="" srcset="">
         </div>
       </div>
     </div>
-  </div>
+  {{-- </div> --}}
 </body>
 </html>
